469a85ab3923fe304867101c9c9fb2e337d9cec6
ArchitTiwary007 <tiwaryart007@gmail.com>
2026-09-07 20:14:37 +0530

    commit_one_today

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,53 @@
 import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+// const reactElem = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target:'_blank'
+//     },
+//     children:'Click to visit google'
+// }
+// const ReactElem = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target:'_blank'
+//     },
+//     children:'Click to visit google'
+// }
+
+const anotherElem = (
+  <a href="https://youtube.com" target='_blank'>VISIT YT</a>
+)
+
+
+const reactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'click me to visit'
+)
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  // <StrictMode>
+    // <MyApp />
+  // </StrictMode>
+
+  // <reactElem />
+  // ReactElem //syntactical error!!
+  reactElement
+
+  // anotherElem//object ye 
 )
