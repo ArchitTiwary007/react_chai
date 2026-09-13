5a2cae42e6cfde35cb0f089b8ef2fb59039d1754
ArchitTiwary007 <tiwaryart007@gmail.com>
2026-09-13 08:06:08 +0530

    initial commit

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-500 text-white'>Currency App with Chai</h1>
+    </>
+  )
+}
+
+export default App
