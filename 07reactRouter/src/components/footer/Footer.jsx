995be48658e2e0e02d5ceeda9a98384ede8d212c
ArchitTiwary007 <tiwaryart007@gmail.com>
2026-09-14 18:45:34 +0530

    router_done

--- 07reactRouter/src/components/footer/Footer.jsx
+++ 07reactRouter/src/components/footer/Footer.jsx
@@ -16,7 +16,7 @@ export default function Footer() {
                     </div>
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
+                            <h2 className="mb-6 text-sm font-semibold text-blue-900 uppercase">Resources</h2>
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <Link to="/" className="hover:underline">
