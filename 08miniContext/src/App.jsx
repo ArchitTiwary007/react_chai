d1d9082bdccc572476831a23fc0d4cc8bd0f0850
ArchitTiwary007 <tiwaryart007@gmail.com>
2026-09-15 13:38:09 +0530

    proj_8 done

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import UserContextProvider from './context/UserContextProvider'
+import Profile from './components/Profile'
 
+import Login from './components/Login'
+
+function App() {
+  
   return (
-    <>
-      <h1>React with Chai & This is For Context API</h1>
-    </>
+    <UserContextProvider>
+      <h1>React with Chai and Context API</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+//user context bana hai
+const UserContext = React.createContext()
+
+export default UserContext;
