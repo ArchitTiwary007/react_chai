ad0291c62a1026be91ccbe63702b533d13e520d2
ArchitTiwary007 <tiwaryart007@gmail.com>
2026-09-20 08:59:54 +0530

    commit_one

---
+++ 11reduxtoolkittodo/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>Learning about redux toolkit</h2>
+    </>
+  )
+}
+
+export default App
