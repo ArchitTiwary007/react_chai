afee65b0d6c8746ba2f882dd1335c18658d59d5c
ArchitTiwary007 <tiwaryart007@gmail.com>
2026-09-25 16:34:43 +0530

    commit_mega_two

--- 12megablog/src/App.jsx
+++ 12megablog/src/App.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice"
+import { Header, Footer } from '../components'
+import {Outlet} from 'react-router-dom'
 
 function App() {
-
-  //playing with environment variable
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-
-  return (
-    <>
-      <h1>A blog app write</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({userData}))
+        } else {
+          dispatch(logout())
+        }
+      })
+    .finally(() => setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          Handle_it:<Outlet />
+        </main>
+        <Footer />
+      </div>
+  </div>
+  ):null
 }
 
 export default App
